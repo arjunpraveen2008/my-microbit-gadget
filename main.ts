2ca6390b249a785fc66b70468af7dd21f749d009
arjunpraveen2008 <74096415+arjunpraveen2008@users.noreply.github.com>
2021-01-08 13:24:51 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+makerbit.onIrButton(IrButton.Number_8, IrButtonAction.Pressed, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+})
+makerbit.connectIrReceiver(DigitalPin.P1, IrProtocol.NEC)
